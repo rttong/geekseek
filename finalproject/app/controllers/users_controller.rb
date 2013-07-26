64701f388f7679678407303feb82eb403789a0cd
Laura Montemayor <laumontemayor@gmail.com>
2013-07-26 16:08:12 -0700

    started with volunteer profile / dashboard
    WIP

--- finalproject/app/controllers/users_controller.rb
+++ finalproject/app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+    if current_user.type == 'V'
+      @volunteer = Volunteer.find_by_user_id(current_user.id)
+      redirect_to 'volunteer/show'
+    else
+      @organization = Organization.find_by_user_id(current_user.id)
+      redirect_to 'organization/show'
 	end
 end
