eba4adce5f956905afdd887febc1bd8dd249bfd1
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-25 19:33:03 -0700

    nav adjustments

--- finalproject/app/models/user.rb
+++ finalproject/app/models/user.rb
@@ -2,10 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-<<<<<<< HEAD
-=======
 
->>>>>>> 9cdba1e50be92d902269c8acea702d61ef351b92
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
