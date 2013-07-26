ad58bad25d2f0f29e5a59cc22d799a4a9e1a77a7
Laura Montemayor <laumontemayor@gmail.com>
2013-07-25 22:04:01 -0700

    sign in - WIP
    Sign in WIP
    Add type to user model

--- finalproject/app/assets/javascripts/sign_in.js
+++ finalproject/app/assets/javascripts/sign_in.js
@@ -1,7 +1,10 @@
-
-$('#sign_in_button').click( function() {            
-    $('#popup_box').fadeIn("slow");
-    debugger;
+$(document).ready(function(){
+    $('#sign_in_button').click( function() {   
+        // console.log('wtf!');
+        // e.preventDefault();
+        // $('#popup_box').find('input').removeAttr('type');
+        $('#popup_box').fadeIn();
+    });
 });
         
         
