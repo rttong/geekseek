7121a259493b43fd8076d7bf899bc3eedb8287bd
Laura Montemayor <laumontemayor@gmail.com>
2013-07-26 14:50:50 -0700

    modified users table / created polymorphic association
    1. Created user table for devise
    2. Created polymorphic associations for organization and volunteers

--- finalproject/app/models/user.rb
+++ finalproject/app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :userable_type, :userable_id
+
+  belongs_to :userable, polymorphic: true
+
 end
--- finalproject/db/migrate/20130725232021_devise_create_users.rb
+++ finalproject/db/migrate/20130725232021_devise_create_users.rb
@@ -1,18 +1,11 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
-      ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
-
-      ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-
-      ## Rememberable
       t.datetime :remember_created_at
-
-      ## Trackable
       t.integer  :sign_in_count, :default => 0
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
@@ -33,7 +26,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Token authenticatable
       # t.string :authentication_token
 
-
+      t.references :userable, polymorphic: true
       t.timestamps
     end
 
