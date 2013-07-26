672d371b93df95cbbdf445d00986f468908685c0
Laura Montemayor <laumontemayor@gmail.com>
2013-07-26 08:51:01 -0700

    javascript for sign in

--- finalproject/app/assets/javascripts/sign_in.js
+++ finalproject/app/assets/javascripts/sign_in.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $('#sign_in_button').click( function() {   
-        console.log('wtf!');
+  $('#sign_in_button').click( function(e) {
+    e.preventDefault();
+    console.log('wtf!');
         // e.preventDefault();
         // $('#popup_box').find('input').removeAttr('type');
-        $('#sign_in_form').fadeIn();
-    });
+    $('#sign_in_form').slideDown();
+  });
 });
-        
-        
+
