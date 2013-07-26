8b35e77ff5bb6a1bd1baaa3d221d97218365610d
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-26 12:25:59 -0700

    added additional user columns

--- finalproject/db/schema.rb
+++ finalproject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130726050229) do
+ActiveRecord::Schema.define(:version => 20130726185715) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(:version => 20130726050229) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "type"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "skills"
+    t.string   "job_history"
+    t.string   "phone"
+    t.string   "availability"
+    t.string   "location"
+    t.string   "portfolio_links"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
