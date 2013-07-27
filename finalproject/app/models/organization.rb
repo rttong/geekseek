867bc9203f0dcde8e26464584b284b083af8e40f
Laura Montemayor <laumontemayor@gmail.com>
2013-07-26 23:31:52 -0700

    javascript for signin/signup completed.
    still needs styling.

--- finalproject/app/models/organization.rb
+++ finalproject/app/models/organization.rb
@@ -1,6 +1,6 @@
 class Organization < ActiveRecord::Base
   # attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :userable_type, :userable_id
-  mount_uploader :avater, AvatarUploader
+  # mount_uploader :avater, AvatarUploader
   has_and_belongs_to_many :causes
   belongs_to :user
 
