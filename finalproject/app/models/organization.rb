6d7e761b345fb252b8fb305cb0e8496f718d3ce4
Laura Montemayor <laumontemayor@gmail.com>
2013-07-27 15:24:42 -0700

    dashboard skeleton configuration

--- finalproject/app/models/organization.rb
+++ finalproject/app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ActiveRecord::Base
   attr_accessible :org_name, :location, :user_id, :cause, :mission, :description, :year_founded, :size, :url, :avatar
 
-  mount_uploader :avater, AvatarUploader
+  # mount_uploader :avater, AvatarUploader
 
   has_and_belongs_to_many :causes
   belongs_to :user
