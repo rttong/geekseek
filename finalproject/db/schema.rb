3997a07952e6b56d5ceed689f3b8bdb3379522ba
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-26 22:43:27 -0700

    org profile partials

--- finalproject/db/schema.rb
+++ finalproject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130727014434) do
+ActiveRecord::Schema.define(:version => 20130727045007) do
 
   create_table "categories", :force => true do |t|
     t.string "website_redesign"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20130727014434) do
     t.integer "year_founded"
     t.integer "size"
     t.string  "url"
+    t.string  "avatar"
   end
 
   create_table "projects", :force => true do |t|
