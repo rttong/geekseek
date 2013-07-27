9b2b09be3bc1de1f6d86fcf714d204e414771128
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-26 20:52:23 -0700

    added join tables

--- finalproject/app/models/organization.rb
+++ finalproject/app/models/organization.rb
@@ -1,6 +1,6 @@
 class Organization < ActiveRecord::Base
   # attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :userable_type, :userable_id
-  has_many :causes
+ has_and_belongs_to_many :causes
   belongs_to :user
 
 end
--- finalproject/app/models/project.rb
+++ finalproject/app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-has_many :categories
+has_and_belongs_to_many :categories
 attr_accessible  :title, :summary, :target_date, :organizations
 end
