07006d6c1d61894ff35bac7f7b0474f32f7f4105
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-27 12:59:51 -0700

    linked view dashboard to correct profile type

--- finalproject/app/controllers/organizations_controller.rb
+++ finalproject/app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 class OrganizationsController < ApplicationController
   before_filter :authenticate_user!
 
-   def index
+  def index
     @users = User.all
   end
 
@@ -9,4 +9,16 @@ class OrganizationsController < ApplicationController
     @user = User.find(params[:id])
     @organization = @user.organization
   end
+
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.create(params[:organization])
+    @organization.tags << tags
+    redirect_to organization_path(@organization)
+  end
+
+
 end
