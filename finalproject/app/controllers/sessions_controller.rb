e0e02d9e08cc2e4057b6c6d799d22b409dfd0fec
Laura Montemayor <laumontemayor@gmail.com>
2013-07-26 18:42:42 -0700

    fixed organization folder

--- finalproject/app/controllers/sessions_controller.rb
+++ finalproject/app/controllers/sessions_controller.rb
@@ -6,5 +6,12 @@ class SessionsController < ApplicationController
   end
 
   def show
+    if current_user.profile_type == 'V'
+      @volunteer = Volunteer.find_by_user_id(current_user.id)
+      redirect_to 'volunteer/show'
+    else
+      @organization = Organization.find_by_user_id(current_user.id)
+      redirect_to 'organization/show'
+    end
   end 
 end
