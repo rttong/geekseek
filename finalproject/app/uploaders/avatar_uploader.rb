a9033cd372061b85043d8fbf8845003b68bac033
Ryan Tong <eufonik@hotmail.com>
2013-07-28 15:32:42 -0700

    trying to pull latest version

--- finalproject/app/uploaders/avatar_uploader.rb
+++ finalproject/app/uploaders/avatar_uploader.rb
@@ -34,9 +34,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   process :scale => [50, 50]
   # end
 
-  version :thumb do
-    process :resize_to_limit => [100, 100]
-  end
+  # version :thumb do
+  #   process :resize_to_limit => [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
