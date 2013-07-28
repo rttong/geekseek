48e1238e0ed6b0fbd78a9d3f8fc97a66957f9104
Laura Montemayor <laumontemayor@gmail.com>
2013-07-27 17:50:57 -0700

    added accordion menu to volunteer

--- finalproject/app/assets/javascripts/dashboard.js
+++ finalproject/app/assets/javascripts/dashboard.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
-  //USERS
-  $('#profile_button').click(function(e) {
-    e.preventDefault();
-    $('.profile').slideDown();
-  });
- 
+
+  $('#dash_menu').accordion();
+
+  var menu_head = $('#dash_menu > li > a'),
+      menu_body = $('#dash_menu li > .submenu');
+
+  menu_head.first().addClass('active').next().slideDown('normal'); 
+  menu_head.on('click',function(event){
+    event.preventDefault();
+
+    if ($(this).attr('class') != 'active'){
+      menu_body.slideUp('normal');
+      $(this).next().stop(true,true).slideDown('normal');
+      menu_head.removeClass('active');
+      $(this).addClass('active');
+    }
+  });   
 });
 
