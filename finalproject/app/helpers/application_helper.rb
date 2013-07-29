2bc0a8d4806d13de2572d2c6b18532813233be53
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-29 14:34:15 -0700

    gravatar done

--- finalproject/app/helpers/application_helper.rb
+++ finalproject/app/helpers/application_helper.rb
@@ -1,12 +1,8 @@
 module ApplicationHelper
   def avatar_url(user)
-    if user.avatar_url.present?
-      user.avatar_url
-    else
-      default_url = "#{root_url}images/fallback/default.png"
-      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-    end
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=200"
   end
 end
+
 
