a850bd384739d3a1031c2c693e505b658d7a6d7e
Ryan Tong <eufonik@hotmail.com>
2013-07-28 20:12:05 -0700

    got jquery image carousel working

--- finalproject/app/assets/javascripts/application.js
+++ finalproject/app/assets/javascripts/application.js
@@ -14,5 +14,3 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require_tree .
-
-
--- finalproject/app/models/cause.rb
+++ finalproject/app/models/cause.rb
@@ -1,4 +1,4 @@
 class Cause < ActiveRecord::Base
   has_and_belongs_to_many :organizations
-attr_accessible :adult_ed, :job_readiness, :special_needs, :animals, :childrens_ed, :environment, :health, :hunger, :seniors, :revitilization, :veterans, :homeless, :lgbt, :other
+	attr_accessible :adult_ed, :job_readiness, :special_needs, :animals, :childrens_ed, :environment, :health, :hunger, :seniors, :revitilization, :veterans, :homeless, :lgbt, :other
 end
