d8646c08f73d5ef0c181054daf77da5387d742a0
Laura Montemayor <laumontemayor@gmail.com>
2013-07-29 22:00:00 -0700

    fixed sign up

--- finalproject/app/models/project.rb
+++ finalproject/app/models/project.rb
@@ -7,9 +7,9 @@ class Project < ActiveRecord::Base
   scope :active, -> { where(state: "active") }
   scope :completed, -> { where(state: "completed") }
 
-  def self.search(params)
+  def self.search(params={})
+    params ||= {}
     relation = self.unscoped
-    return relation unless params[:cause_ids].present? || params[:causes_ids].present?
     relation = self.joins(organization: :causes).where("causes.id IN (?)", params[:cause_ids]) if params[:cause_ids]
     relation = relation.joins(:categories).where("categories.id IN (?)", params[:category_ids]) if params[:category_ids]
     # relation.where("title ILIKE('%?%') OR summary ILIKE('%?%')", params[:text],params[:text]) if params[:text]
