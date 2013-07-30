d3364dc7cdadeb2603b787363a5abd5c7231b531
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-30 15:57:41 -0700

    added user model specs

--- finalproject/spec/models/user_spec.rb
+++ finalproject/spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
-describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe User do
+
+    let(:email) { "johndoe.@gmail.com" }
+    let(:password) { "password" }
+    let(:user) { User.create(email: username, password: password, password_confirmation: password }
+
+    it "should require a email to create a user" do
+      expect{ User.create!(email: "", password: password) }.to raise_error
+    end
+
+    it "should require a password to create a user" do
+      expect{ User.create!(email: email, password: "") }.to raise_error
+    end
+
+    it "should ensure proper validation of email and password" do
+      user.should be_valid
+    end
+
+    it "should create a user object when data is valid" do
+      User.find(user.id).should eq user
+    end
+
+    it "should not be valid if email is duplicate" do
+      user
+      User.create(email: email, password: password).should be_invalid
+    end
+  end
