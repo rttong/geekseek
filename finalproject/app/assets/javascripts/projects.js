60ded47915ea62796112a8635fc27853ad57f876
Laura Montemayor <laumontemayor@gmail.com>
2013-07-29 21:27:18 -0700

    fancy box sucks

--- finalproject/app/assets/javascripts/projects.js
+++ finalproject/app/assets/javascripts/projects.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $('#search_form').on('ajax:success', function(event,data,xhr){
     $('.content').replaceWith($(data).find('.content'));
   });
-  
+
+  $(".fancybox").fancybox();
+  $("#sign_in_form").fancybox();
 });
 
