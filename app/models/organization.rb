3b6ae9764d0d31f06ef59f3d5799677f046a0750
cemmanuel1 <cemmanuel1@gmail.com>
2013-07-31 01:07:28 -0700

    working on org project page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,7 @@ class Organization < ActiveRecord::Base
   attr_accessible :org_name, :location, :user_id, :cause, :mission, :description, :year_founded, :size, :url, :avatar, :cause_ids
   belongs_to :user
   has_and_belongs_to_many :causes
+  has_many :volunteers, through: :projects
   has_many :projects
   accepts_nested_attributes_for :causes
   mount_uploader :avatar, AvatarUploader
