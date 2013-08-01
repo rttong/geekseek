993c64b506afd3fcf18e26323af0ef4bf57a304c
cemmanuel1 <cemmanuel1@gmail.com>
2013-08-01 00:58:17 -0700

    fixed and resized thumbnail to 100X100

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -31,7 +31,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [150, 150]
+    process :resize_to_fit => [100, 100]
   end
 
   # version :thumb do
