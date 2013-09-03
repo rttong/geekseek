1f0a0b7149a3cf370af58d4c7f164ba7eef246a8
Laura Montemayor <laumontemayor@gmail.com>
2013-09-03 14:51:13 -0700

    added new relic gem

--- app/assets/javascripts/interest.js
+++ app/assets/javascripts/interest.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
   $('#accept_volunteer').on('ajax:success', function(event,data,xhr){
-
-    alert("WORK~~~~~~!!!!!!")
     $('#content_right').replaceWith($(data).find('#content_right'));
   });
 });
--- app/assets/javascripts/sign_in.js
+++ app/assets/javascripts/sign_in.js
@@ -1,35 +1,5 @@
 $(document).ready(function(){
 
-  // $('.sign_in_button').click(function(e) {
-  //   e.preventDefault();
-  //   $('#sign_in_form').fadeIn();
-  // });
-
-  // $('.sign_in_close').click(function(e) {
-  //   e.preventDefault();
-  //   $('#sign_in_form').fadeOut();
-  // });
-
-  // $('#signup_v').click( function(e) {
-  //   e.preventDefault();
-  //   $('#signup_form_v').fadeIn();
-  // });
-
-  // $('#signup_v_close').click(function(e) {
-  //   e.preventDefault();
-  //   $('#signup_form_v').fadeOut();
-  // });
-
-  // $('#signup_org').click( function(e) {
-  //   e.preventDefault();
-  //   $('#signup_form_org').fadeIn();
-  // });
-
-  // $('#signup_org_close').click(function(e) {
-  //   e.preventDefault();
-  //   $('#signup_form_org').fadeOut();
-  // });
-
   $(".fancybox").fancybox();
 
 });
