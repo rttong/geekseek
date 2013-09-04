86b2d84ac928b31d80ebb3604f45e43ceef26b40
Laura Montemayor <laumontemayor@gmail.com>
2013-09-03 19:45:24 -0700

    error message display in sign in box

--- app/assets/javascripts/sign_in.js
+++ app/assets/javascripts/sign_in.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
   $(".fancybox").fancybox();
-
-});
+  $("#sign_in_form").bind("ajax:error", function(evt, data, status, xhr){
+    $(".sign_in_error").removeClass('hidden');
+  });
+  });
 
